bbade81b9d6c57914321280d73238ad3855b1e00
johnnynode <johnnynode@gmail.com>
2017-10-09 09:06:54 +0800

    project init

---
+++ README.md
@@ -0,0 +1,9 @@
+requirejs-avalon1.4.7-SPA-seed
+===
+
+
+### 参考的网址和例子：
+- http://ued.qunar.com/oniui/mmRouter/avalon.mmRouter.doc.html
+- https://github.com/gogoyqj/mmRouter-demo-list
+- https://github.com/gogoyqj/mmRouter-demo-list/blob/master/requirejs/www/script/lib/oniui/mmRouter/mmState.js
+- https://github.com/gogoyqj/mmRouter-demo-list/blob/master/requirejs/www/script/lib/oniui/mmRouter/mmRouter.js
---
+++ main.js
@@ -0,0 +1,28 @@
+﻿// 配置
+require.config({
+    baseUrl: './',
+    paths: {
+        avalon: ["lib/avalon/avalon"],
+        mmHistory: ['lib/avalon/mmHistory'],
+        mmRouter: ['lib/avalon/mmRouter/mmRouter'],
+        mmState: ['lib/avalon/mmRouter/mmState'],
+        mmPromise: ['lib/avalon/mmPromise/mmPromise'],
+        text: ['lib/require/text'],
+        css: ['lib/require/css'],
+        jquery: ['lib/jquery/jquery-1.12.4'],
+        jqueryCookie: ['lib/jquery/jquery.cookie-1.4.1'],
+        domReady: ['lib/require/domReady']
+    },
+    shim: {
+        avalon: {exports: "avalon"},
+        mmHistory: {deps: ['avalon']},
+        mmRouter: {deps: ['avalon']}
+    },
+    priority: ['text']
+});
+
+// 引入路由
+require(['avalon', 'domReady!'],
+    function (avalon) {
+        //
+    });
