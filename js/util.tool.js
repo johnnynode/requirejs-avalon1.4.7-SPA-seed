d5c2034eaa6af4af06ade3d24b559b6d90a76b88
johnnynode <johnnynode@gmail.com>
2017-10-17 08:29:45 +0800

    add IE detect

--- js/util.tool.js
+++ js/util.tool.js
@@ -2,6 +2,22 @@
 define(['avalon', 'jquery'],
     function (avalon, $) {
         return {
-
+            /* 检测浏览器的版本 */
+            isLowIE: function () {
+                return !$.support.leadingWhitespace || this.isIE9(); // !$.support.leadingWhitespace 用于检测IE6-8
+            },
+            /* 检测IE特殊版本IE9 */
+            isIE9: function () {
+                // 先判断是否是IE
+                if (/msie/.test(navigator.userAgent.toLowerCase())) {
+                    // 检测浏览器的版本
+                    var browser = navigator.appName;
+                    var b_version = navigator.appVersion;
+                    var version = b_version.split(";");
+                    var trim_Version = version[1].replace(/[ ]/g, "");
+                    return (trim_Version === "MSIE9.0"); // 再来检测IE 9
+                }
+                return false;
+            },
         }
     });
